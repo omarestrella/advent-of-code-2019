f655c6ead052b80a1aac96bc88ae8a3cb39f7637
Omar Estrella <omarestrella@bitcreative.net>
2019-12-02 19:35:38 -0500

    Day 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./css/style.less";
 
 import day1 from "./days/1";
+import day2 from "./days/2";
 
 function getSolution(day) {
   switch (day) {
     case 1:
       return day1();
+    case 2:
+      return day2();
     default:
       return "shrug...";
   }
