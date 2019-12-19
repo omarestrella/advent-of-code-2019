34591e53f69df8a69d399332d3bee91fe1073967
Omar Estrella <omarestrella@bitcreative.net>
2019-12-18 23:30:06 -0500

    Part 2...sigh.

--- src/days/6.js
+++ src/days/6.js
@@ -1,6 +1,7 @@
 import input from "./inputs/6.txt";
 import * as R from "ramda";
 
+// eslint-disable-next-line no-unused-vars
 const TEST_INPUT = `COM)B
 B)C
 C)D
@@ -11,15 +12,18 @@ G)H
 D)I
 E)J
 J)K
-K)L`;
+K)L
+K)YOU
+I)SAN`;
 
 function parseInput(str) {
   return str.split("\n").map(pair => pair.trim().split(")"));
 }
 
-function makePlanet(name) {
+function makePlanet(name, parent) {
   return {
     name,
+    parent,
     orbits: []
   };
 }
@@ -34,6 +38,23 @@ function findPlanet(head, name) {
   }
 
   return findPlanet(head.orbits[0], name) || findPlanet(head.orbits[1], name);
+}
+
+function countToRoot(head, name, count = 0) {
+  if (!head) {
+    return null;
+  }
+
+  if (head.name === name) {
+    return count;
+  }
+
+  const first = countToRoot(head.orbits[0], name, count + 1);
+  if (first) {
+    return first;
+  }
+
+  return countToRoot(head.orbits[1], name, count + 1);
 }
 
 function countOrbits(head, seen = 0) {
@@ -61,7 +82,8 @@ function buildPlanets(nodes, head = {}, nodeToFind = "COM") {
       orbits: [
         {
           name: orbit,
-          orbits: []
+          orbits: [],
+          parent: head
         }
       ]
     };
@@ -77,24 +99,55 @@ function buildPlanets(nodes, head = {}, nodeToFind = "COM") {
   if (first) {
     const [planetName, orbit] = first;
     const planet = findPlanet(head, planetName);
-    planet.orbits.push(makePlanet(orbit));
+    planet.orbits.push(makePlanet(orbit, planet));
 
     buildPlanets(nodes, head, orbit);
   }
   if (second) {
     const [planetName, orbit] = second;
     const planet = findPlanet(head, planetName);
-    planet.orbits.push(makePlanet(orbit));
+    planet.orbits.push(makePlanet(orbit, planet));
 
     buildPlanets(nodes, head, orbit);
   }
 }
 
+function commonOrbit(head, planetA, planetB) {
+  if (!head) {
+    return null;
+  }
+  if (head === planetA || head === planetB) {
+    return head;
+  }
+
+  const leftRoot = commonOrbit(head.orbits[0], planetA, planetB);
+  const rightRoot = commonOrbit(head.orbits[1], planetA, planetB);
+
+  if (leftRoot && rightRoot) {
+    return head;
+  }
+
+  return leftRoot || rightRoot;
+}
+
+function distanceBetween(head, planetAName, planetBName) {
+  const planetA = findPlanet(head, planetAName);
+  const planetB = findPlanet(head, planetBName);
+  const orbit = commonOrbit(head, planetA, planetB);
+
+  const countA = countToRoot(orbit, planetAName);
+  const countB = countToRoot(orbit, planetBName);
+
+  return countA + countB - 2;
+}
+
 export default function run() {
   // const data = parseInput(TEST_INPUT);
   const data = parseInput(input);
-  const tree = buildPlanets(data);
-  const count = countOrbits(tree) - 1; // Subtract for COM
+  const head = buildPlanets(data);
 
-  return [`Day 1: ${count}`].join("\n");
+  const count = countOrbits(head) - 1; // Subtract for COM
+  const distance = distanceBetween(head.orbits[0], "YOU", "SAN");
+
+  return [`Part 1: ${count}`, `Part 2: ${distance}`].join("\n");
 }
